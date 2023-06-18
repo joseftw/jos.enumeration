2b6e2e274f64f8432739536da264c90740c024ce
joseftw <josef@localhost.local>
2023-06-18 12:40:52 +0200

    Adds example consumer of the nuget package in a test project

---
+++ test/ExampleNugetConsumer/Pizza.cs
@@ -0,0 +1,11 @@
+using JOS.Enumeration;
+
+namespace ExampleNugetConsumer;
+
+public partial record Pizza : IEnumeration<Pizza>
+{
+    public static readonly Pizza Vesuvio = new(1, "Vesuvio");
+    public static readonly Pizza Calzone = new(2, "Calzone");
+    public static readonly Pizza Hawaii = new(3, "Hawaii");
+    public static readonly Pizza Kebab = new(4, "Kebab");
+}
