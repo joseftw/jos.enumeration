21d5d7ed824171228d40b2b5d2f2397e31f23448
joseftw <josef@localhost.local>
2024-11-08 09:38:39 +0100

    Adds support for nameof and string concatenation

---
+++ test/JOS.Enumerations/Drink.cs
@@ -0,0 +1,10 @@
+using JOS.Enumeration;
+using System;
+
+namespace JOS.Enumerations;
+
+public partial class Drink : IEnumeration<Drink>
+{
+    public static readonly Drink GinTonic = new(1, "Gin" + "Tonic");
+    public static readonly Drink RedBullVodka = new(2, nameof(RedBullVodka));
+}
