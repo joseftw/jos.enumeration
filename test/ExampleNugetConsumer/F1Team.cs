83ea14ed5ecae0b4eb3a426a07b16f908e9b18e5
jos-yoda <144553819+jos-yoda@users.noreply.github.com>
2024-12-30 09:46:35 +0000

    Fixes formatting - 4.1.17-beta-gc3199f59ca

--- test/ExampleNugetConsumer/F1Team.cs
+++ test/ExampleNugetConsumer/F1Team.cs
@@ -4,6 +4,6 @@ namespace ExampleNugetConsumer;
 
 public partial class F1Team : IEnumeration<F1Team>
 {
-    public static readonly F1Team RedBull = new (1, "Red Bull");
+    public static readonly F1Team RedBull = new(1, "Red Bull");
     public static readonly F1Team Mercedes = new F1Team(2, "Mercedes");
 }
