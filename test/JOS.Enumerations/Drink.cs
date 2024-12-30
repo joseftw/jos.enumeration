e2424720d27f0c20fb67a169dadc3a171c503fa3
joseftw <joseftw@users.noreply.github.com>
2024-12-30 14:24:07 +0100

    Wrong ordering of usings
    Added whitespace
    
    Don't merge

--- test/JOS.Enumerations/Drink.cs
+++ test/JOS.Enumerations/Drink.cs
@@ -5,6 +5,6 @@ namespace JOS.Enumerations;
 
 public partial class Drink : IEnumeration<Drink>
 {
-    public static readonly Drink GinTonic = new(1, "Gin" + "Tonic");
+    public static readonly Drink GinTonic = new (1, "Gin" + "Tonic");
     public static readonly Drink RedBullVodka = new(2, nameof(RedBullVodka));
 }
