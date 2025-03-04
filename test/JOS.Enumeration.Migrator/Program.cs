eadc79bd43927152e7b3bee96ef95a8ded98f857
joseftw <joseftw@users.noreply.github.com>
2025-03-04 09:10:27 +0100

    Try System.CommandLine

--- test/JOS.Enumeration.Migrator/Program.cs
+++ test/JOS.Enumeration.Migrator/Program.cs
@@ -4,16 +4,17 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.CommandLine;
 
 var builder = Host.CreateDefaultBuilder(args)
-    .ConfigureAppConfiguration((_, configurationBuilder) =>
-    {
-        configurationBuilder.AddJsonFile("appsettings.json");
-        configurationBuilder.AddJsonFile("appsettings.Development.json", optional: true);
-        configurationBuilder.AddEnvironmentVariables();
-        configurationBuilder.AddEnvironmentVariables("JOS_Enumeration_");
-        configurationBuilder.AddCommandLine(args);
-    });
+                  .ConfigureAppConfiguration((_, configurationBuilder) =>
+                  {
+                      configurationBuilder.AddJsonFile("appsettings.json");
+                      configurationBuilder.AddJsonFile("appsettings.Development.json", optional: true);
+                      configurationBuilder.AddEnvironmentVariables();
+                      configurationBuilder.AddEnvironmentVariables("JOS_Enumeration_");
+                      configurationBuilder.AddCommandLine(args);
+                  });
 builder.UseDefaultServiceProvider(options =>
 {
     options.ValidateScopes = true;
@@ -26,16 +27,12 @@ builder.ConfigureServices(services =>
 });
 
 var app = builder.Build();
-
-var hostEnv = app.Services.GetRequiredService<IHostEnvironment>();
-var logger = app.Services.GetRequiredService<ILogger<Program>>();
-logger.LogInformation("Running in {HostEnv} mode", hostEnv.EnvironmentName);
-var scope = app.Services.CreateAsyncScope();
-
-await using(scope)
+var migrateCommand = new Command("migrate", "Migrates the database");
+migrateCommand.SetHandler(async _ =>
 {
+    await using var scope = app.Services.CreateAsyncScope();
     var dbContexts = scope.ServiceProvider.GetServices<DbContext>();
-
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
     foreach(var dbContext in dbContexts)
     {
         var dbContextName = dbContext.GetType().Name;
@@ -43,4 +40,13 @@ await using(scope)
         await dbContext.Database.MigrateAsync();
         logger.LogInformation("Migration of {DbContextName} done", dbContextName);
     }
-}
+});
+var rootCommand = new RootCommand("JOS.Enumeration.Migrator");
+rootCommand.SetHandler(x =>
+{
+    using var scope = app.Services.CreateAsyncScope();
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+    logger.LogInformation("Hello world");
+});
+rootCommand.AddCommand(migrateCommand);
+return await rootCommand.InvokeAsync(args);
