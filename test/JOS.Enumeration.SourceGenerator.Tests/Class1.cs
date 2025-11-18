befbd6a482121bd4ad22c2639191ee9f67e2deb9
Josef Ottosson <joseftw@users.noreply.github.com>
2025-11-18 13:29:26 +0100

    Fixes for xunit and MTP

---
+++ test/JOS.Enumeration.SourceGenerator.Tests/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Xunit;
+
+namespace JOS.Enumeration.SourceGenerator.Tests;
+
+public class Class1
+{
+    [Fact]
+    public void TestGenerator()
+    {
+        var source = @"your test code";
+        var generator = new EnumerationSourceGenerator();
+
+        GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
+        var compilation = CSharpCompilation.Create("test",
+            new[] { CSharpSyntaxTree.ParseText(source, cancellationToken: TestContext.Current.CancellationToken) });
+
+        driver.RunGeneratorsAndUpdateCompilation(compilation, out _, out _, TestContext.Current.CancellationToken);
+    }
+}
